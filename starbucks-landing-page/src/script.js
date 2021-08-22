fdc86c98d5e00d113f384b3bf645eb9c99a4babd
Babatounde Bohiki <xivmilbillions@gmail.com>
2021-08-22 11:16:23 +0100

    starbucks landing page website using tailwindcss

---
+++ starbucks-landing-page/src/script.js
@@ -0,0 +1,7 @@
+function changeImg(theimg) {
+  document.getElementById("starbucks").src = theimg;
+}
+
+function changeCircle(color) {
+  document.querySelector(".circle").style.backgroundColor = color;
+}
